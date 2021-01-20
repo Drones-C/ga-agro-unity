fe2186c16fbdf87966f1badfdc615f52aa5ec5b8
Sasha <lonagi22@gmail.com>
2021-01-21 00:33:41 +0200

    Destroyer

--- Assets/Plant.cs
+++ Assets/Plant.cs
@@ -9,3 +9,12 @@ public class Plant : MonoBehaviour
     public float water;
     public float water_capacity;
     public List<int> Genome = new List<int>();
+
+    private IEnumerator MySecUpdater()
+    {
+        yield return new WaitForSeconds(1);
+        water--;
+        if(water<=0)
+            Destroy(gameObject);
+    }
+
