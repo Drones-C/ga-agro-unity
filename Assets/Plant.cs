6edad1fdbedcbd173e0ff24e303940167fe42da3
Sasha <lonagi22@gmail.com>
2021-01-21 00:33:18 +0200

    Plant obj

---
+++ Assets/Plant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Plant : MonoBehaviour
+{
+    public float water;
+    public float water_capacity;
+    public List<int> Genome = new List<int>();
