75995f034a5dfac2817d2f4867ff32b7ef2f92f1
Sasha <lonagi22@gmail.com>
2021-01-21 00:34:49 +0200

    Crop field

---
+++ Assets/MakeField.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeField : MonoBehaviour
+{
+    public Transform x1;
+    public Transform x2;
+
+    public GameObject Dirt;
+    public GameObject[] Crops;
+    public List<GameObject> Population = new List<GameObject>();
