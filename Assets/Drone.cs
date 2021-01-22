63062f53c4f86926f0f5c4a6683ec37c423a56d3
Sasha <lonagi22@gmail.com>
2021-01-22 14:36:20 +0200

    Take off drone

---
+++ Assets/Drone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(Vector3.up / 50,Space.Self);
+    }
+}
