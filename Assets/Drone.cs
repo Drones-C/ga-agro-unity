6437b183e992d03e140506318a59e6b932e547a1
Sasha <lonagi22@gmail.com>
2021-01-22 15:09:33 +0200

    take off to coroutine

--- Assets/Drone.cs
+++ Assets/Drone.cs
@@ -5,28 +5,38 @@ using UnityEngine;
 
 public class Drone : MonoBehaviour
 {
-    private Vector3 flight_vector;
     public short speed;
 
+    private bool gained_high = false;
+
+    private void Flight(Vector3 flight_vector)
+    {
+        transform.Translate(flight_vector,Space.Self);
+    }
+
+    private IEnumerator TakeOff()
+    {
+        yield return new WaitForSeconds(Time.deltaTime);
+        Vector3 flight_vector = new Vector3(0,Time.deltaTime * speed,0);
+        Flight(flight_vector);
+
+        if (transform.position.y < 60)
+            StartCoroutine(TakeOff());
+        else
+            gained_high = true;
+    }
+    
     private void Start()
     {
         speed = 5;
+        StartCoroutine(TakeOff());
     }
 
-    void Update()
+    private void LateUpdate()
     {
-        //Takeoff
-        if (transform.position.y < 60)
+        if (gained_high)
         {
-            flight_vector = new Vector3(0,Time.deltaTime * speed,0);
+            //
         }
-        else
-        {
-            flight_vector = Vector3.zero;   
-        }
-        
-        
-        //Flight
-        transform.Translate(flight_vector,Space.Self);
     }
 }
