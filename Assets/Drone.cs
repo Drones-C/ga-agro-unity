0a99d16a6f77472a3960c43c93c69c837b282321
Sasha <lonagi22@gmail.com>
2021-01-22 14:54:26 +0200

    Take off planning

--- Assets/Drone.cs
+++ Assets/Drone.cs
@@ -5,8 +5,28 @@ using UnityEngine;
 
 public class Drone : MonoBehaviour
 {
+    private Vector3 flight_vector;
+    public short speed;
+
+    private void Start()
+    {
+        speed = 5;
+    }
+
     void Update()
     {
-        transform.Translate(Vector3.up / 25,Space.Self);
+        //Takeoff
+        if (transform.position.y < 60)
+        {
+            flight_vector = new Vector3(0,Time.deltaTime * speed,0);
+        }
+        else
+        {
+            flight_vector = Vector3.zero;   
+        }
+        
+        
+        //Flight
+        transform.Translate(flight_vector,Space.Self);
     }
 }
